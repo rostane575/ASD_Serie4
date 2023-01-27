1bfe230fecf04f43f223c4b86625feb435c753cf
Kenza ROSTANE <68350477+rostane575@users.noreply.github.com>
2023-01-27 00:56:23 -1200

    Update Exo4.c

--- Exo4.c
+++ Exo4.c
@@ -1,45 +1,66 @@
+/*On veut compter le nombre d'occurrences d'une
+valeur VAL donnée dans chaque ligne d'une
+matrice.
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
+
 int main()
 {
-    int n;
-    printf("Taille du tableau ");
+    /*1. EAQP d'afficher le nombre d'occurrences pour
+chaque colonne sans utiliser de tableau
+supplémentaire.*/
+    int n, m, val, oc;
+
+    printf("Donnez le nombre de lignes SVP!\n");
     scanf("%d", &n);
-    int T[n], T_pos[n], T_neg[n];
-    for (int i = 0; i < n; i++)
-    {
-        printf("T[%d]= ", i);
-        scanf("%d", &T[i]);
 
-    }
-    printf("le tableau initial:\n");
-    for (int i = 0; i < n; i++)
+    printf("Donnez le nombre de colonnes SVP!\n");
+    scanf("%d", &m);
+
+    int T[n][m];
+
+    printf("Donner la valeur recherchée\n");
+    scanf("%d", &val);
+
+    printf("Remplissage de la matrice:\n");
+    for (int j = 0; j < m; j++)
     {
-        printf("T[%d] = %d , ", i, T[i]);
-    }
-    int h = 0, k = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (T[i] > 0)
+        oc = 0;
+        for (int i = 0; i < n; i++)
+
         {
-            T_pos[h] = T[i];
-            h++;
+            printf("T[%d][%d] = ", i, j);
+            scanf("%d", &T[i][j]);
+            if (T[i][j] == val)
+            {
+                oc = oc + 1;
+            }
         }
-        else
-        {
-            T_neg[k] = T[i];
-            k++;
-        }
-    }
-    printf("\nle tableau positif:\n");
-    for (int i = 0; i < h; i++)
-    {
-        printf("T_pos[%d] = %d , ", i, T_pos[i]);
+        printf("Le nombre d’occurrences à la colonne =%d est %d\n", j, oc);
     }
 
-    printf("\nle tableau negatif:\n");
-    for (int i = 0; i < k; i++)
+    /*2. Modifier l'algorithme précédent pour qu'il
+sauvegarde ces nombres d'occurrences dans un
+tableau avant de les afficher.*/
+    printf("\n*******************************************\n");
+    int T_oc[n];
+    int i, j;
+    for (j = 0; j < m; j++)
+
     {
-        printf("T_neg[%d] = %d , ", i, T_neg[i]);
+        T_oc[j] = 0;
+        for (i = 0; i < n; i++)
+        {
+            if (T[i][j] == val)
+            {
+                T_oc[j] = T_oc[j] + 1;
+            }
+        }
+    }
+    for (j = 0; j < m; j++)
+    {
+
+        printf("Le nombre d’occurrences à la colonne =%d est %d\n", j, T_oc[j]);
     }
 }
